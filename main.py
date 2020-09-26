70f9e8049554f3006bab56311abb3d9d3c70a3d7
asimmirza <asim.j.mirza@gmail.com>
2020-09-27 00:33:33 +0530

    Added pipeline message

--- main.py
+++ main.py
@@ -3,7 +3,12 @@
 import os
 import subprocess
 
+os.system("clear")
+username = subprocess.check_output("whoami",shell=True)
+msg = "Hello " + username.decode("ascii")
+print("msg")
 print("Welcome to CI-CT Application \n\n")
+ip =0
 while True:
     print("CI-CT Option:")
     print("1. Test Data")
@@ -12,4 +17,19 @@ while True:
     if ip==1 or ip==2:
         break
     else:
-        print("INVALID CHOICE")
+        os.system("clear")
+        print("INVALID CHOICE \n")
+
+
+if ip==2:
+    while True:
+        print("You have choose : pipeline")
+        print("1. Create new pipeline")
+        print("2. Clone existing pipeline")
+        print("3. Run Pipeline")
+        ip = int(input("Please Enter your choice:"))
+        if ip==1 or ip==2 or ip==3:
+            break
+        else:
+            os.system("clear")
+            print("INVALID CHOICE \n")
